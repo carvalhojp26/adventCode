557a3211ca81f8f29704cdcb1d23075e22e75d53
Joao Paulo Carvalho <acarvalho.jp@gmail.com>
2024-12-02 23:53:54 +0000

    starting day 1

---
+++ Day_1/Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        File file = new File("./input.txt");
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+
+        while ((st = br.readLine()) != null) {
+            System.out.println(st);
+        }
+    }
+}
