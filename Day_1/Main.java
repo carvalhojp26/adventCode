db8979486db095b01682664bc8544f42995f9cc9
Joao Paulo Carvalho <acarvalho.jp@gmail.com>
2024-12-04 01:14:19 +0000

    Day 1 part 1 resolution

--- Day_1/Main.java
+++ Day_1/Main.java
@@ -1,15 +1,45 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         File file = new File("./input.txt");
 
-        BufferedReader br = new BufferedReader(new FileReader(file));
+        if (file.exists()) {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            
+            String line;
 
-        String st;
+            ArrayList<Integer> firstColumn = new ArrayList<>();
+            ArrayList<Integer> secondColumn = new ArrayList<>();
 
-        while ((st = br.readLine()) != null) {
-            System.out.println(st);
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("\\s+");
+
+
+                firstColumn.add(Integer.parseInt(parts[0]));
+                secondColumn.add(Integer.parseInt(parts[1]));
+            }
+            Collections.sort(firstColumn);
+            Collections.sort(secondColumn);
+            
+            int sum = 0;
+
+            for (int i = 0; i < firstColumn.size(); i++) {
+                int subtraction = firstColumn.get(i) - secondColumn.get(i);
+
+                if (subtraction < 0) {
+                    subtraction = subtraction * -1;
+                }
+
+                sum += subtraction;
+            }
+
+            System.out.println("Sum: " + sum);
+            br.close();
+        } else {
+            System.out.println("File not found");
         }
     }
 }
